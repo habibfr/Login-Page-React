db6b956ea5cbb4945d5ae862847023460dc34f6e
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-02-19 11:43:12 +0700

    useRef and useImperetiveHandler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+  useRef,
+} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -40,6 +46,9 @@ const Login = () => {
       console.log("Effect Clean Up");
     };
   }, []);
+
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const authCtx = useContext(AuthContext);
 
@@ -98,13 +107,21 @@ const Login = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!formIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           isValid={emailIsValid}
           type="email"
           id="email"
@@ -115,6 +132,7 @@ const Login = () => {
         />
 
         <Input
+          ref={passwordInputRef}
           isValid={passwordIsValid}
           type="password"
           id="password"
@@ -124,7 +142,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
